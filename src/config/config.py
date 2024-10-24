222d6075d5dc069229c1e3b07847a518ac83ca93
unknown <bernardoboggionechamilet@gmail.com>
2024-10-24 16:24:56 -0300

    adição pacote de configuração

---
+++ src/config/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# Caminho absoluto até o diretório principal onde o .env está localizado
+env_path = Path(__file__).resolve().parent.parent.parent / '.env'
+
+# Carregando variáveis de ambiente
+load_dotenv(dotenv_path=env_path)
+
+port = int(os.getenv('PORT'))
+
+debug = bool(os.getenv('DEBUG'))
