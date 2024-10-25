33a251d05950f4146e41bb8d5dace83fa3d7e394
unknown <bernardoboggionechamilet@gmail.com>
2024-10-25 13:58:35 -0300

    Adição de cofirma senha no modelo de usuário para cadastro

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -1,15 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 # Classe para validação de dados de usuário 
 class Usuario(BaseModel):
     usuario: str = Field(..., min_length=7)
     nome: str = Field(..., min_length=2)
     senha: str = Field(..., min_length=6)
+    confirma: str = Field(..., min_length=6)
 
-# Classe para validação de dados de atualização de senha
-class Senhas(BaseModel):
-    senha_atual: str = Field(..., min_length=6)
-    senha_nova: str = Field(..., min_length=6)
+    @model_validator(mode='after')
+    def verificaSenhas(cls, values):
+        if values.senha != values.confirma:
+            raise ValueError('As senhas não são iguais')
+        return values
 
 # Classe para validação de campos no login
 class Login(BaseModel):
