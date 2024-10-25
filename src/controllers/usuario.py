234ff996b96f919cd695a0c1dad314d8260bb3aa
unknown <bernardoboggionechamilet@gmail.com>
2024-10-25 14:00:54 -0300

    Implementação da rota de cadastro

--- src/controllers/usuario.py
+++ src/controllers/usuario.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, request, render_template
+import src.models.usuario as models
+from pydantic import ValidationError
+import requests
+from src.config.config import apiurl
 
 usuario_bp = Blueprint('usuario', __name__)
 
@@ -9,18 +13,27 @@ def cadastro():
 	if request.method == 'POST':
 		# Obtendo dados enviados pelo formulário
 		dados = request.form
-		#usuario = request.form.get('usuario')
-		#nome = request.form.get('nome')
-		#senha = request.form.get('senha')
-		#confirma = request.form.get('confirma')
-		
-        # Fracasso
-		msgErro = "Senhas não coincidem"
-		msgErro = "Usuário já existe"
-		return render_template("cadastro.html", msgErro=msgErro, dadosForm=dados)
-		
-        # Sucesso
-		return render_template("login.html",sucesso=True)
+		# Validando dados
+		try:
+			usuario = models.Usuario(**dados)
+		except ValidationError as e:
+			msgErro = "Preencha os campos corretamente"
+			return render_template("cadastro.html", msgErro=msgErro, dadosForm=dados)
+		usuarioDict = usuario.model_dump()
+		# Fazendo requisição a API
+		respostaAPI = requests.post(f'{apiurl}/usuarios', json=usuarioDict)
+		if respostaAPI.status_code == 201:
+			# Sucesso 201
+			return render_template("login.html",sucesso=True)
+		elif respostaAPI.status_code == 409:
+			msgErro = "Usuário já existe" #409
+			return render_template("cadastro.html", msgErro=msgErro, dadosForm=dados)
+		elif respostaAPI.status_code == 400:
+			msgErro = "Preencha os campos corretamente" #400
+			return render_template("cadastro.html", msgErro=msgErro, dadosForm=dados)
+		else:
+			msgErro = "Ocorreu um erro em nosso servidor, tente novamente" #500
+			return render_template("cadastro.html", msgErro=msgErro, dadosForm=dados)
 	# Renderização da página de cadastro
 	if request.method == 'GET':
 		return render_template("cadastro.html", dadosForm={})
